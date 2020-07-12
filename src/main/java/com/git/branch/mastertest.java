3cec4717d1fdd299fd62f9a8ec5eb09e63fbfb7a
qinwei <57479220+qinweifly@users.noreply.github.com>
2020-07-12 19:04:01 +0800

    Qw modify pri (#2)
    * num3属性修改
    
    * first test
    
    * second test add get func

--- src/main/java/com/git/branch/mastertest.java
+++ src/main/java/com/git/branch/mastertest.java
@@ -3,6 +3,14 @@ package com.git.branch;
 public class mastertest {
     Integer num = 666;
     int num2 = 888;
-    String num3 = "7777.00";
-    int point = 1;
+    Long num3 = 7777L;
+
+    String name = "test-rebase";
+
+    public mastertest() {
+    }
+
+    public String getName() {
+        return name;
+    }
 }
---
+++ src/main/java/com/git/branch/rebaseTest.java
@@ -0,0 +1,17 @@
+package com.git.branch;
+
+public class rebaseTest {
+    private String name ;
+    private String id ;
+
+    public rebaseTest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
